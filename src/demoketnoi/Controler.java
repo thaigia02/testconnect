e8cd4e8db2bf0beb46d9b432e3398ed9bbba0403
thaigia02 <thaigialh02@gmail.com>
2024-05-30 13:25:42 +0700

    kết nối SQl

---
+++ src/demoketnoi/Controler.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demoketnoi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Controler {
+    public static Connection getJDBCConnection(){
+        String url = "jdbc:mysql://localhost:3306/test";
+        String user = "root";
+        String password ="1234";
+        
+    
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(url,user,password);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Controler.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Controler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+        
+    }
+    public static void main(String[] args) {
+        Connection conn = getJDBCConnection();
+        if(conn != null){
+            System.out.println("Ket noi tc");
+        }else{
+            System.out.println("Thất bại");
+        }
+    }
+
+    }
+        
+
+        
+    
